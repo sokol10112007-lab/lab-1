b8406caeae5c90086dd3e2e9976bf81af11f8ab0
sokol10112007-lab <sokol10112007@gmail.com>
2026-09-21 22:39:51 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LissajousLab
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
